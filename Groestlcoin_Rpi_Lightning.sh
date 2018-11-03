5efddb3127876d3b4d9d6d8a5dd6d48a8340dd59
Gruve_p <gruve-p@users.noreply.github.com>
2018-11-03 14:54:30 +0100

    Update and rename Bitcoin_Rpi_Lightning.sh to Groestlcoin_Rpi_Lightning.sh

--- Groestlcoin_Rpi_Lightning.sh
+++ Groestlcoin_Rpi_Lightning.sh
@@ -5,16 +5,18 @@ sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev
 
 # For development or running tests, get additional dependencies:
 sudo apt-get install -y asciidoc valgrind python3-pip
-sudo pip3 install python-bitcoinlib
 
 # Clone lightning
-git clone https://github.com/ElementsProject/lightning.git
+git clone https://github.com/Groestlcoin/lightning.git
 cd lightning
+
+# Configure Lightning
+./configure
 
 # Build lightning
 make
 
 # Running lightning
-bitcoind &
+groestlcoind &
 ./lightningd/lightningd &
 ./cli/lightning-cli help
